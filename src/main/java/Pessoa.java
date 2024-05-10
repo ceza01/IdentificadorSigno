2693446ced5c955054f8e6110c8b769a078587fe
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-05-10 01:57:54 -0300

    adicionando classe

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,22 @@
+import java.time.Period;
+import java.util.Date;
+
+public class Pessoa {
+    private Date dataDeNascimento;
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public void getIdade(){
+
+    }
+
+    public void getSigno(){
+
+    }
+}
